a60defd594a9289dd78091c410fdab374c1803e8
LisaXie <lxie.ger@gmail.com>
2016-02-14 10:39:14 +0530

    New Project

---
+++ iBeaconFuckEstimotes/app/src/main/java/yolognese/com/ibeaconfuckestimotes/MainActivity.java
@@ -0,0 +1,15 @@
+package yolognese.com.ibeaconfuckestimotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    }
+}
