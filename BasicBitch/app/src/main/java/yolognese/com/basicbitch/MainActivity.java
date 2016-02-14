19a34dc294293084f16ef7de88351fc843a45fc6
LisaXie <lxie.ger@gmail.com>
2016-02-14 10:20:45 +0530

    initial commit with kind of dindgy workig ranging;

---
+++ BasicBitch/app/src/main/java/yolognese/com/basicbitch/MainActivity.java
@@ -0,0 +1,63 @@
+package yolognese.com.basicbitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
+
+import java.util.List;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BeaconManager beaconManager;
+    private Region region;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                if (!list.isEmpty()) {
+                    Beacon nearestBeacon = list.get(0);
+                    Log.d("Beacon Debug", list.toString());
+                    // TODO: update the UI here
+                    Log.d("Beacon closeness", "Closest Beacon is " + nearestBeacon.getMeasuredPower() + " or "+ nearestBeacon.getRssi());
+                }
+            }
+        });
+
+        region = new Region("ranged region",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        beaconManager.stopRanging(region);
+
+        super.onPause();
+    }
+
+}
